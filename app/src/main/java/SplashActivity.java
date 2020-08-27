9c6c52d66d2c025807f6b5bc05f5ef523ba8f7b3
nooranimeisyaroh30 <nooranimeisyaroh7@gmail.com>
2020-08-27 11:59:00 +0700

    responsii

---
+++ app/src/main/java/SplashActivity.java
@@ -0,0 +1,12 @@
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+}
